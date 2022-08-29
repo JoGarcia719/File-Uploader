c6ef8472c271d315b05165f49825f47d789ae14c
JoGarcia719 <83441976+JoGarcia719@users.noreply.github.com>
2022-08-29 20:51:25 +0800

    Add files via upload

---
+++ uploader.php
@@ -0,0 +1,12 @@
+<?php
+$destination = C:/xampp/htdocs/pdc10/file-uploader/" . $ _FILES['userfile']['name'];
+
+$tmp = $_FILES['userfile']['tmp_name'];
+
+if (is_uploaded_file($tmp)) {
+	if (move_uploaded_file($tmp, $destination)) {
+		echo 'File is successfully uploaded';
+		exit();
+	}
+}
+echo 'Unable to upload file';
